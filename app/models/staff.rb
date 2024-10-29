b0cbdc290076c8803b52f79da1b03edf19226214
Ekosmolak <ekosmolak@gmail.com>
2024-10-29 18:35:42 -0500

    Added Faker

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,2 +1,3 @@
 class Staff < ApplicationRecord
+  validates :first_name, :last_name, :position, presence: true
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,5 @@
 class Vehicle < ApplicationRecord
+  validates :manufacturer, :model, :colour, :price, :odometer, :vehicle_type, presence: true
+  validates :price, :odometer, numericality: true
+  belongs to :manufacturer
 end
